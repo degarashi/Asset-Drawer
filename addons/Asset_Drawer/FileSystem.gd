5cd0595e88ac29bcfbcfb92280359a1561adc5de
Degarashi <degarasu@gmail.com>
2026-01-04 16:40:02 +0900

    [update] File system dock component encapsulation
    Encapsulation of file system dock components into a new `FileDockInfo` class. This improves code organization and centralizes access to the dock, split container, tree, and content container. It removes direct member variables for UI elements.

--- addons/Asset_Drawer/FileSystem.gd
+++ addons/Asset_Drawer/FileSystem.gd
@@ -10,12 +10,25 @@ const BOTTOM_PADDING: int = 60
 ## Minimum height of the dock
 const MIN_HEIGHT: int = 50
 
-## The file system
-var file_dock: FileSystemDock = null
 
-var file_split_container: SplitContainer = null
-var file_tree: Tree = null
-var file_container: VBoxContainer = null
+## The file system
+class FileDockInfo:
+	var dock: FileSystemDock
+	var split_container: SplitContainer
+	var tree: Tree
+	var container: VBoxContainer
+
+	func _init(d: FileSystemDock) -> void:
+		dock = d
+		split_container = dock.get_child(3) as SplitContainer
+		tree = split_container.get_child(0) as Tree
+		container = split_container.get_child(1) as VBoxContainer
+		assert(split_container)
+		assert(tree)
+		assert(container)
+
+
+var fd_info: FileDockInfo
 var asset_drawer_shortcut: InputEventKey = InputEventKey.new()
 
 ## Toggle for when the file system is moved to bottom
@@ -29,27 +42,20 @@ func _enter_tree() -> void:
 	# Add tool button to move shelf to editor bottom
 	add_tool_menu_item("Files to Bottom", files_to_bottom)
 
-	init_file_dock()
+	# Get our file system
+	fd_info = FileDockInfo.new(EditorInterface.get_file_system_dock())
 
 	await get_tree().create_timer(0.1).timeout
 	files_to_bottom()
 
 	# Prevent file tree from being shrunk on load
 	await get_tree().create_timer(0.1).timeout
-	file_split_container.split_offset = 175
+	fd_info.split_container.split_offset = 175
 
 	# Get shortcut
 	asset_drawer_shortcut = (
 		preload("res://addons/Asset_Drawer/AssetDrawerShortcut.tres") as InputEventKey
 	)
-
-
-func init_file_dock() -> void:
-	# Get our file system
-	file_dock = EditorInterface.get_file_system_dock()
-	file_split_container = file_dock.get_child(3) as SplitContainer
-	file_tree = file_split_container.get_child(0) as Tree
-	file_container = file_split_container.get_child(1) as VBoxContainer
 
 
 #region show hide filesystem
@@ -61,7 +67,7 @@ func _input(event: InputEvent) -> void:
 		if showing:
 			hide_bottom_panel()
 		else:
-			make_bottom_panel_item_visible(file_dock)
+			make_bottom_panel_item_visible(fd_info.dock)
 
 		showing = not showing
 
@@ -75,33 +81,33 @@ func _exit_tree() -> void:
 
 
 func _process(_delta: float) -> void:
-	var window := file_dock.get_window()
+	var window := fd_info.dock.get_window()
 	new_size = window.size
 
 	# Keeps the file system from being unusable in size
 	if window.name == ROOT and not files_bottom:
-		file_tree.size.y = new_size.y - PADDING
-		file_container.size.y = new_size.y - PADDING
+		fd_info.tree.size.y = new_size.y - PADDING
+		fd_info.container.size.y = new_size.y - PADDING
 		return
 
 	# Adjust the size of the file system based on how far up
 	# the drawer has been pulled
 	if window.name == ROOT and files_bottom:
-		var dock_container := file_dock.get_parent() as Control
+		var dock_container := fd_info.dock.get_parent() as Control
 		new_size = dock_container.size
 		var editorsettings := EditorInterface.get_editor_settings()
 		var fontsize: int = editorsettings.get_setting("interface/editor/main_font_size")
 		var editorscale := EditorInterface.get_editor_scale()
 
-		file_tree.size.y = new_size.y - (fontsize * 2) - (BOTTOM_PADDING * editorscale)
-		file_container.size.y = new_size.y - (fontsize * 2) - (BOTTOM_PADDING * editorscale)
+		fd_info.tree.size.y = new_size.y - (fontsize * 2) - (BOTTOM_PADDING * editorscale)
+		fd_info.container.size.y = new_size.y - (fontsize * 2) - (BOTTOM_PADDING * editorscale)
 		return
 
 	# Keeps our systems sized when popped out
 	if window.name != ROOT and not files_bottom:
 		window.min_size.y = MIN_HEIGHT
-		file_tree.size.y = new_size.y - PADDING
-		file_container.size.y = new_size.y - PADDING
+		fd_info.tree.size.y = new_size.y - PADDING
+		fd_info.container.size.y = new_size.y - PADDING
 
 		# Centers window on first pop
 		if not initial_load:
@@ -113,12 +119,12 @@ func _process(_delta: float) -> void:
 # Moves the files between the bottom panel and the original dock
 func files_to_bottom() -> void:
 	if files_bottom:
-		remove_control_from_bottom_panel(file_dock)
-		add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BR, file_dock)
+		remove_control_from_bottom_panel(fd_info.dock)
+		add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BR, fd_info.dock)
 		files_bottom = false
 		return
 
-	init_file_dock()
-	remove_control_from_docks(file_dock)
-	add_control_to_bottom_panel(file_dock, "File System")
+	fd_info = FileDockInfo.new(EditorInterface.get_file_system_dock())
+	remove_control_from_docks(fd_info.dock)
+	add_control_to_bottom_panel(fd_info.dock, "File System")
 	files_bottom = true
